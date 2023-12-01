02a70ca662c3aca7beef004c2194baeaa0703712
Cory Douglas <coryddouglas@gmail.com>
2023-12-01 15:35:49 -0500

    part 2 completion

--- AdventofCSharp2023/Day1.cs
+++ AdventofCSharp2023/Day1.cs
@@ -2,7 +2,7 @@
 {
     public static class Day1
     {
-        public static int SumOfCalibrationValues(IEnumerable<string> calibrationInput)
+        public static int SumOfCalibrationValuesDigitsOnly(IEnumerable<string> calibrationInput)
         {
             int sum = 0;
 
@@ -17,7 +17,7 @@
 
                 int? firstDigit = null;
                 int? firstIndex = null;
-                for(int i = 0; i < input.Length; i++)
+                for (int i = 0; i < input.Length; i++)
                 {
                     if (Char.IsNumber(input[i]))
                     {
@@ -60,5 +60,110 @@
 
             return sum;
         }
+
+        public static int SumOfCalibrationValues(IEnumerable<string> calibrationInput)
+        {
+            int sum = 0;
+
+            foreach (var input in calibrationInput)
+            {
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                int toAdd = 0;
+
+                int? firstDigit = null;
+                int? firstIndex = null;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    if (Char.IsNumber(input[i]))
+                    {
+                        firstDigit = input[i] - '0';
+                        firstIndex = i;
+                        break;
+                    }
+
+                    for (int j = i; j >= 0; j--)
+                    {
+                        var word = input.Substring(j, (i - j) + 1);
+                        if (wordToIntDictionary.ContainsKey(word))
+                        {
+                            firstDigit = wordToIntDictionary[word];
+                            firstIndex = j;
+                            break;
+                        }
+                    }
+
+                    if (firstDigit != null)
+                    {
+                        break;
+                    }
+                }
+
+                if (firstDigit == null || firstIndex == null)
+                {
+                    continue;
+                }
+
+                if (firstIndex == input.Length)
+                {
+                    toAdd = (firstDigit.Value * 10) + firstDigit.Value;
+                    sum += toAdd;
+                    continue;
+                }
+
+                int? secondDigit = null;
+                for (int i = input.Length - 1; i > firstIndex.Value; i--)
+                {
+                    if (Char.IsNumber(input[i]))
+                    {
+                        secondDigit = input[i] - '0';
+                        break;
+                    }
+
+                    for (int j = input.Length - 1; j >= i; j--)
+                    {
+                        var word = input.Substring(i, (j - i) + 1);
+                        if (wordToIntDictionary.ContainsKey(word))
+                        {
+                            secondDigit = wordToIntDictionary[word];
+                            break;
+                        }
+                    }
+
+                    if (secondDigit != null)
+                    {
+                        break;
+                    }
+                }
+
+                if (secondDigit == null)
+                {
+                    secondDigit = firstDigit;
+                }
+
+                toAdd = (firstDigit.Value * 10) + secondDigit.Value;
+                sum += toAdd;
+            }
+
+            return sum;
+        }
+
+        private static Dictionary<string, int> wordToIntDictionary = new Dictionary<string, int>()
+        {
+            { "zero", 0 },
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+        };
     }
+
 }
--- AdventofCSharp2023_Tests/Day1Tests.cs
+++ AdventofCSharp2023_Tests/Day1Tests.cs
@@ -11,7 +11,7 @@ namespace AdventofCSharp2023_Tests
 
 
         [Test]
-        public void SampleTest()
+        public void SampleTest_DigitsOnly()
         {
             var input = new List<string>()
             {
@@ -20,7 +20,34 @@ namespace AdventofCSharp2023_Tests
                 "a1b2c3d4e5f",
                 "treb7uchet"
             };
-            Assert.That(Day1.SumOfCalibrationValues(input), Is.EqualTo(142));
+            Assert.That(Day1.SumOfCalibrationValuesDigitsOnly(input), Is.EqualTo(142));
+        }
+
+        [Test]
+        public void Day1InputTest_DigitsOnly()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Day1", "Day1Input.txt");
+            var input = File.ReadLines(path).ToList();
+
+            var answer = Day1.SumOfCalibrationValuesDigitsOnly(input);
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void SampleTest()
+        {
+            var input = new List<string>()
+            {
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen"
+            };
+            Assert.That(Day1.SumOfCalibrationValues(input), Is.EqualTo(281));
         }
 
         [Test]
