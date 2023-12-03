712148daff7ddbf84e67eacf7fb4a4895704a22b
Cory Douglas <coryddouglas@gmail.com>
2023-12-03 13:00:53 -0500

    Day 3 part 2

--- AdventofCSharp2023/Day3.cs
+++ AdventofCSharp2023/Day3.cs
@@ -27,10 +27,10 @@ namespace AdventofCSharp_2023
 
                     if(!Char.IsNumber(lines[i][charIdx]) || charIdx == lines[i].Length - 1)
                     {
-                        if (!string.IsNullOrEmpty(currentNumberString))
-                        {
-                            Debug.WriteLine($"Current number string: {currentNumberString}; IsValid: {currentNumberIsValid}");
-                        }
+                        //if (!string.IsNullOrEmpty(currentNumberString))
+                        //{
+                        //    Debug.WriteLine($"Current number string: {currentNumberString}; IsValid: {currentNumberIsValid}");
+                        //}
                         if(!string.IsNullOrEmpty(currentNumberString) && currentNumberIsValid)
                         {
                             sum += int.Parse(currentNumberString);
@@ -42,6 +42,148 @@ namespace AdventofCSharp_2023
                 }
             }
             return sum;
+        }
+
+        public static int SumOfPartNumbersV2(List<string> lines)
+        {
+            var linesArray = lines.Select(i => i.ToArray()).ToArray();
+            int sum = 0;
+            for (int i = 0; i < linesArray.Length; i++)
+            {
+                for (int charIdx = 0; charIdx < linesArray[i].Length; charIdx++)
+                {
+                    if (!Char.IsNumber(linesArray[i][charIdx]) && linesArray[i][charIdx] != '.')
+                    {
+                        var adjacentNumbers = GetAdjacentNumbers(linesArray, i, charIdx);
+
+                        sum += adjacentNumbers.Sum();
+                    }
+                }
+            }
+            return sum;
+        }
+
+        public static int SumOfGears(List<string> lines)
+        {
+            var linesArray = lines.Select(i => i.ToArray()).ToArray();
+            int sum = 0;
+            for (int i = 0; i < linesArray.Length; i++)
+            {
+                for (int charIdx = 0; charIdx < linesArray[i].Length; charIdx++)
+                {
+                    if (linesArray[i][charIdx] == '*')
+                    {
+                        var adjacentNumbers = GetAdjacentNumbers(linesArray, i, charIdx);
+
+                        if(adjacentNumbers.Count() == 2)
+                        {
+                            sum += adjacentNumbers.Aggregate((x, y) => x * y);
+                        }
+                    }
+                }
+            }
+            return sum;
+        }
+
+        private static List<int> GetAdjacentNumbers(char[][] lines, int lineIdx, int charIdx)
+        {
+            var ret = new List<int>();
+
+            if (lineIdx != 0)
+            {
+                //check up
+                if (Char.IsNumber(lines[lineIdx - 1][charIdx]))
+                {
+                    ret.Add(GetFullNumber(lines, lineIdx - 1, charIdx));
+                }
+
+                //up left
+                if (charIdx != 0)
+                {
+                    if (Char.IsNumber(lines[lineIdx - 1][charIdx - 1]))
+                    {
+                        ret.Add(GetFullNumber(lines, lineIdx - 1, charIdx - 1));
+                    }
+                }
+
+                //up right
+                if (charIdx != lines[0].Length - 1)
+                {
+                    if (Char.IsNumber(lines[lineIdx - 1][charIdx + 1]))
+                    {
+                        ret.Add(GetFullNumber(lines, lineIdx - 1, charIdx + 1));
+                    }
+                }
+            }
+
+            //left
+            if (charIdx != 0)
+            {
+                if (Char.IsNumber(lines[lineIdx][charIdx - 1]))
+                {
+                    ret.Add(GetFullNumber(lines, lineIdx, charIdx - 1));
+                }
+            }
+
+            //right
+            if (charIdx != lines[0].Length - 1)
+            {
+                if (Char.IsNumber(lines[lineIdx][charIdx + 1]))
+                {
+                    ret.Add(GetFullNumber(lines, lineIdx, charIdx + 1));
+                }
+            }
+
+            if (lineIdx != lines.Length - 1)
+            {
+                //check down
+                if (Char.IsNumber(lines[lineIdx + 1][charIdx]))
+                {
+                    ret.Add(GetFullNumber(lines, lineIdx + 1, charIdx));
+                }
+
+                //down left
+                if (charIdx != 0)
+                {
+                    if (Char.IsNumber(lines[lineIdx + 1][charIdx - 1]))
+                    {
+                        ret.Add(GetFullNumber(lines, lineIdx + 1, charIdx - 1));
+                    }
+                }
+
+                //down right
+                if (charIdx != lines[0].Length - 1)
+                {
+                    if (Char.IsNumber(lines[lineIdx + 1][charIdx + 1]))
+                    {
+                        ret.Add(GetFullNumber(lines, lineIdx + 1, charIdx + 1));
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        private static int GetFullNumber(char[][] lines, int lineIdx, int charIdx)
+        {
+            var currIdx = charIdx;
+
+            while (currIdx >= 0 && Char.IsNumber(lines[lineIdx][currIdx]))
+            {
+                currIdx--;
+            }
+            currIdx++;
+
+            var currentNumberString = string.Empty;
+            while (currIdx < lines[lineIdx].Length && Char.IsNumber(lines[lineIdx][currIdx]))
+            {
+                currentNumberString += lines[lineIdx][currIdx];
+                lines[lineIdx][currIdx] = '.';
+
+                currIdx++;
+            }
+
+            return int.Parse(currentNumberString);
         }
 
         private static bool HasAdjacentSymbol(List<string> lines, int lineIdx, int charIdx)
--- AdventofCSharp2023_Tests/Day3Tests.cs
+++ AdventofCSharp2023_Tests/Day3Tests.cs
@@ -26,6 +26,27 @@ namespace AdventofCSharp2023_Tests
             Assert.That(ret, Is.EqualTo(4361));
         }
 
+        public void SampleInputV2()
+        {
+            var input = new List<string>()
+            {
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598.."
+            };
+
+            var ret = Day3.SumOfPartNumbersV2(input);
+
+            Assert.That(ret, Is.EqualTo(4361));
+        }
+
         [Test]
         public void Day3InputTest()
         {
@@ -33,6 +54,39 @@ namespace AdventofCSharp2023_Tests
             var input = File.ReadLines(path).ToList();
 
             var answer = Day3.SumOfPartNumbers(input);
+
+            Assert.True(true);
+        }
+
+        [Test]
+        public void SampleInputPart2()
+        {
+            var input = new List<string>()
+            {
+                "467..114..",
+                "...*......",
+                "..35..633.",
+                "......#...",
+                "617*......",
+                ".....+.58.",
+                "..592.....",
+                "......755.",
+                "...$.*....",
+                ".664.598.."
+            };
+
+            var ret = Day3.SumOfGears(input);
+
+            Assert.That(ret, Is.EqualTo(467835));
+        }
+
+        [Test]
+        public void Day3InputTestPart2()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Day3", "Day3Input.txt");
+            var input = File.ReadLines(path).ToList();
+
+            var answer = Day3.SumOfGears(input);
 
             Assert.True(true);
         }
