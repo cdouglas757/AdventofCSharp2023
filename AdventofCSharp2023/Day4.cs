b96f8cb869dbf667d6cd11cb9a058e299fa07352
Cory Douglas <coryddouglas@gmail.com>
2023-12-04 10:34:52 -0500

    day 4 part 2

--- AdventofCSharp2023/Day4.cs
+++ AdventofCSharp2023/Day4.cs
@@ -32,5 +32,45 @@
 
             return sum;
         }
+
+        public static int SumOfTotalCardsAfterWinning(IEnumerable<string> cards)
+        {
+            Dictionary<int, int> cardCount = new Dictionary<int, int>();
+            foreach (var card in cards)
+            {
+                var cardInfo = card.Split(':');
+                var cardId = int.Parse(cardInfo[0].Split(" ").Where(i => !string.IsNullOrEmpty(i)).Last());
+
+                if (!cardCount.ContainsKey(cardId)) cardCount.Add(cardId, 1);
+                else cardCount[cardId]++;
+
+                var numbers = cardInfo[1].Split("|");
+
+                var winningNumbers = numbers[0].Trim().Split(" ").Where(i => !string.IsNullOrEmpty(i)).Select(i => int.Parse(i)).ToList();
+                var playingNumbers = numbers[1].Trim().Split(" ").Where(i => !string.IsNullOrEmpty(i)).Select(i => int.Parse(i)).ToList();
+
+                var winningCount = 0;
+                foreach (var number in playingNumbers)
+                { 
+                    if (winningNumbers.Contains(number))
+                    {
+                        winningCount++;
+                    }
+                }
+
+                var wonCardId = cardId;
+                var wonCardAmount = cardCount[cardId];
+                for(int i = 0; i < winningCount; i++)
+                {
+                    wonCardId++;
+
+                    if (!cardCount.ContainsKey(wonCardId)) cardCount.Add(wonCardId, wonCardAmount);
+                    else cardCount[wonCardId] += wonCardAmount;
+                }
+
+            }
+
+            return cardCount.Sum(cc => cc.Value);
+        }
     }
 }
