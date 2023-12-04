d52d3f5231f366fe24342db47f0dfb26dee57dc9
Cory Douglas <coryddouglas@gmail.com>
2023-12-04 10:19:35 -0500

    day 4 part 1

---
+++ AdventofCSharp2023/Day4.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventofCSharp_2023
+{
+    public static class Day4
+    {
+        public static int SumOfWinningCardValues(IEnumerable<string> cards)
+        {
+            var sum = 0;
+            foreach (var card in cards)
+            {
+                var numbers = card.Split(':')[1].Split("|");
+
+                var winningNumbers = numbers[0].Trim().Split(" ").Where(i => !string.IsNullOrEmpty(i)).Select(i => int.Parse(i)).ToList();
+                var playingNumbers = numbers[1].Trim().Split(" ").Where(i => !string.IsNullOrEmpty(i)).Select(i => int.Parse(i)).ToList();
+
+                var cardValue = 0;
+                foreach(var number in playingNumbers)
+                {
+                    if (winningNumbers.Contains(number))
+                    {
+                        if (cardValue == 0)
+                        {
+                            cardValue = 1;
+                            continue;
+                        }
+
+                        cardValue *= 2;
+                    }
+                }
+
+                sum += cardValue;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ AdventofCSharp2023_Tests/Day4Tests.cs
@@ -0,0 +1,36 @@
+﻿using AdventofCSharp_2023;
+
+namespace AdventofCSharp2023_Tests
+{
+    public class Day4Tests
+    {
+        [Test]
+        public void Day4SampleInput()
+        {
+            var input = new List<string>()
+            {
+                "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+                "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+                "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+                "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+                "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+                "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+            };
+
+            var ret = Day4.SumOfWinningCardValues(input);
+
+            Assert.That(ret, Is.EqualTo(13));
+        }
+
+        [Test]
+        public void Day4InputTest()
+        {
+            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "TestData", "Day4", "Day4Input.txt");
+            var input = File.ReadLines(path).ToList();
+
+            var answer = Day4.SumOfWinningCardValues(input);
+
+            Assert.True(true);
+        }
+    }
+}
